527847b236b0e05b7b621fbb7204a3c470a32313
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:39:05 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// madrid-protocol - Madrid Protocol toolkit - International trademark registration via WIPO. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
